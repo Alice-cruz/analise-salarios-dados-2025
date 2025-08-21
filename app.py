82262b1e70e79f70e864dc832ec82997e748fb1b
Alice-cruz <alice.cr314@gmail.com>
2025-08-20 22:17:51 -0300

    criacao do grafico dos salarios

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ st.set_page_config(
 def load_data():
     return pd.read_parquet('data/dados_limpos.parquet')
 
-data_load_state = st.text('Loading data...')
-df = load_data()
-data_load_state.text("Done!")
+with st.spinner('Loading data...'):
+    df = load_data()
+st.success("Dados carregados corretamente!")
 
 ############# filtro da barra lateral ########################
 # barra lateral com o nome
@@ -23,7 +23,7 @@ st.sidebar.header("🔍 Filtros")
 
 # Filtro de Ano
 anos_disponiveis_df = sorted(df['ano'].unique())
-anos_selected = st.sidebar.multiselect('Anos', anos_disponiveis_df, default=[])
+anos_selected = st.sidebar.multiselect('Anos', anos_disponiveis_df, default= max(anos_disponiveis_df))
 
 # Filtro de senioridade
 
@@ -39,3 +39,64 @@ tamanho_companhia = sorted(df['tamanho_empresa'].unique())
 tamanho_empresa_selected = st.sidebar.multiselect('Tamanho da Empresa', tamanho_companhia, default=tamanho_companhia)
 
 ################################################################
+##### df filtrado com base nos filtros criados acima
+df_filtrado = df[
+    (df['ano'].isin(anos_selected)) &
+    (df['nivel_experiencia'].isin(senioridade_selected)) &
+    (df['tipo_contratacao'].isin(contrato_selected)) &
+    (df['tamanho_empresa'].isin(tamanho_empresa_selected))
+]
+
+#### formatacao pagina inicial
+
+st.title('🎲 Dashboard: Analise de Salários na Area de Dados')
+st.markdown('Explore as informações. Lembresse de fazer os filtros na aba lateral')
+
+##### kpis
+
+st.subheader('Metricas de Salarios Anuais (USD)')
+
+if not df_filtrado.empty:
+    salario_medio = df_filtrado['salario_em_usd'].mean()
+    salario_max = df_filtrado['salario_em_usd'].max()
+    total_registros = df_filtrado.shape[0]
+    ## moda do cargo comum pegando o 1 item
+    cargo_mais_comum = df_filtrado['cargo'].mode()[0]
+else:
+    salario_medio,salario_max, total_registros, cargo_mais_comum = 0,0,0,""
+
+# cards na home
+col1, col2, col3, col4= st.columns(4)
+
+col1.metric("Salario Medio", f"${salario_medio:,.0f}")
+col2.metric("Maior Salario", f'${salario_max:,.0f}')
+col3.metric("Total de Campos", f"{total_registros:,}")
+col4.metric("Cargo Mais Comum", cargo_mais_comum)
+st.markdown("---")
+##################
+
+###insercao dos gratifocs interativos
+st.subheader("Gráficos")
+
+#with abre a coluna onde o grafico vai ser plotado
+
+col1_grap1, col2_grap2 = st.columns(2)
+
+with col1_grap1:
+    if not df_filtrado.empty:
+        top_cargos = df.groupby('cargo')['salario_em_usd'].mean().nlargest(10).sort_values(ascending=True).reset_index()
+        graf_cargos = px.bar(
+            top_cargos,
+            x='salario_em_usd',
+            y='cargo',
+            orientation='h',
+            title='Top 10 Salários - Média (USD)',
+            labels={'salario_em_usd': "Media salarial", 'cargo': ""}
+
+        )
+        graf_cargos.update_layout(title_x=0.5, yaxis={'categoryorder':'total ascending'})
+        st.plotly_chart(graf_cargos, use_container_width=True)
+    else:
+        st.warning("Nenhum dado para exibir no gráfico de cargos.")
+
+
